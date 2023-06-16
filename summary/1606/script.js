bb8640c8e38dd49c33ca2ae73035a29d9e35104f
ValeriaKul <lera.dribnyuk@mail.ru>
2023-06-16 12:42:16 +0200

    16.06

--- summary/1606/script.js
+++ summary/1606/script.js
@@ -17,8 +17,6 @@ formInput.forEach((elem) => {
 
 btn.onclick = saveForm;
 
-
-
 function handlerChange(e) {
     userStore[e.target.name] = e.target.value;
 }
@@ -36,4 +34,108 @@ function saveForm() {
 
     div.append(img, h6);
     document.querySelector('.user').append(div);
+}
+
+class Element {
+    constructor(type, className) {
+        this.type = type;
+        this.className = className;
+        this.styles = {};
+    }
+    create(){
+        let {type, className, styles} = this;
+
+        let elem = document.createElement('input');
+            elem.setAttribute("type", type);
+            for (const key in styles) {
+                // console.log(styles[key]);
+                elem.style[key] = styles[key];
+            }
+        let div = document.createElement("div");
+            div.append(elem);   
+            document.querySelector(`.${className}`).append(div)
+    }
+    addStyle(styles){
+        this.styles = styles;
+    }
+}
+
+// let textElement = new Element("text", "elements");
+// textElement.create();
+
+// let numberElement = new Element('number', 'elements');
+// numberElement.create();
+
+class TextElement extends Element {
+    constructor(type, className){
+        super(type, className)
+        this.type = 'text';
+        this.className = 'elements'
+    }
+}
+
+let p1 = new TextElement();
+p1.addStyle({color: "red", border: "1px solid green"});
+p1.create();
+
+class NumberElement extends Element {
+    constructor(type, className){
+        super(type, className)
+        this.type = 'number';
+        this.className = 'elements'
+    }
+}
+
+let p2 = new NumberElement()
+p2.addStyle({color: "red", border: "1px solid green"});
+p2.create();
+
+let p3 = new TextElement();
+p3.addStyle({
+    color: "#000", 
+    border: "1px solid #000", 
+    padding: "8px 16px", 
+    backgroundColor: "#ccc",
+    "font-size": "18px" 
+})
+p3.create();
+
+class DateElement extends Element {
+    constructor(type, className){
+        super(type, className)
+        this.type = 'date';
+        this.className = 'elements'
+    }
+}
+
+class ColorElement extends Element {
+    constructor(type, className){
+        super(type, className)
+        this.type = 'color';
+        this.className = 'elements'
+    }
+}
+
+class TimeElement extends Element {
+    constructor(type, className){
+        super(type, className)
+        this.type = 'time';
+        this.className = 'elements'
+    }
+}
+
+class CheckboxElement extends Element {
+    constructor(type, className){
+        super(type, className)
+        this.type = 'checkbox';
+        this.className = 'elements'
+    }
+}
+
+class RadioElement extends Element {
+    constructor(type, className){
+        super(type, className)
+        this.type = 'radio';
+        this.className = 'elements'
+    }
 }
